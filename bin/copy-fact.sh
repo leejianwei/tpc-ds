88d67d30540fbd38f30dbdf6ea9ada1b8fd98aec
leejianwei <leejianwei@126.com>
2021-01-12 12:14:16 +0000

    update script

--- bin/copy-fact.sh
+++ bin/copy-fact.sh
@@ -2,11 +2,11 @@
 
 FACTS="store_sales store_returns web_sales web_returns catalog_sales catalog_returns inventory"
 
-mkdir data/fact
+mkdir ../data/fact
 for t in ${FACTS}
 do
 for file_name in `ls ../data/${t}*.dat`; do
-	cp $file_name data/fact/
+	cp $file_name ../data/fact/
 done
 
 done
